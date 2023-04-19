9a6b015f79373cd249a3a4bdac345c8d56effec5
isadoradg <isahdomingos011@gmail.com>
2023-04-19 14:38:30 -0300

    Insersão de um menu lateral com o login do usuário (nome e email), fonte de pesquisa, favoritos e perfil. Também foi colocado no canto superior direito um icone de logout do usuário

--- app_projeto_pratico/lib/view/tela_login.dart
+++ app_projeto_pratico/lib/view/tela_login.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, camel_case_types
 
+import 'package:app_projeto_pratico/view/tela_principal.dart';
 import 'package:flutter/material.dart';
 
 //
@@ -19,6 +20,17 @@ class _TelaLoginState extends State<TelaLogin> {
       appBar: AppBar(
         //title: Text('Gastronomy'),
         backgroundColor: const Color.fromARGB(255, 188, 138, 60),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.logout,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
       ),
       body: Container(
         padding: EdgeInsets.all(20),
@@ -46,7 +58,22 @@ class _TelaLoginState extends State<TelaLogin> {
               onPressed: () {
                 //Navigator.pop(context);
               }, 
-              child: Text("Receitas"), //abrir uma nova tela com as receitas diponíveis e seu conteúdo(detalhe)
+              child: Text("Pesquisar Receita..."), //abrir uma nova tela com as receitas diponíveis e seu conteúdo(detalhe)
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+              ),
+              onPressed: () {
+                //Navigator.pop(context);
+              }, 
+              child: const Text("Receitas"),
             ),
 
             SizedBox(
@@ -73,25 +100,26 @@ class _TelaLoginState extends State<TelaLogin> {
                 backgroundColor: Colors.white,
                 foregroundColor: Colors.black,
               ),
+              // Usando AlertDialog para apresentar sobre o aplicativo
+              /*onPressed: () => showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Sobre o Aplicativo'),
+                  content: const Text('A escolha do tema voltado para culinária foi devido ao meu gosto por cozinhar. O objetivo da criação desse aplicativo é ser prático e auxiliar na busca por receitas. Esse App foi desenvolvido pela Isadora Domingos na disciplina de Programação para Dispositivos Móveis. /foto/',),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'OK'),
+                      child: const Text('OK'),
+                    ),
+                  ],
+                ),
+              ), */
+              
               onPressed: () {
                 //Navigator.pop(context);
-              }, 
-              child: const Text("Configurações"),
-            ),
-
-            SizedBox(
-              height: 10,
-            ),
-
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.black,
-              ),
-              onPressed: () {
-                //Navigator.pop(context);
-                sobreAplicativo(context);
-              }, 
+                //sobreAplicativo();
+                Navigator.pushNamed(context, '/sobre');
+              },
               child: const Text("Sobre"), 
             ),
 
@@ -101,10 +129,85 @@ class _TelaLoginState extends State<TelaLogin> {
         ),
       ),
 
+      drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255,235,196,127),
+                ),
+                accountName: Text("Taylor Scott"),
+                accountEmail: Text("tscott@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,//Color.fromARGB(255,235,196,127),
+                    /*Theme.of(context).platform == TargetPlatform.iOS
+                    ? Color.fromARGB(255,235,196,127): Colors.black,*/
+                    child: Text("T", style: 
+                      TextStyle(fontSize: 40.0, color: Colors.black),
+                    ),
+                ),
+              ),
+              ListTile(
+                  //leading: Icon(Icons.search)),
+                  title: Text("Pesquisar Receita"),
+                  //subtitle: Text("Perfil do usuário..."),
+                  trailing: Icon(Icons.search),
+                  onTap: () {
+                    //Navigator.pop(context);
+                  }),
+              ListTile(
+                  leading: Icon(Icons.star),
+                  title: Text("Favoritos"),
+                  //subtitle: Text("meus favoritos..."),
+                  //trailing: Icon(Icons.arrow_forward),
+                  onTap: () {
+                    //debugPrint('toquei no drawer');
+                  }),
+              ListTile(
+                  leading: Icon(Icons.account_circle),
+                  title: Text("Perfil"),
+                  //subtitle: Text("Perfil do usuário..."),
+                  //trailing: Icon(Icons.arrow_forward),
+                  onTap: () {
+                    //Navigator.pop(context);
+                  }),
+              
+            ],
+          ),
+      ),
+
     );
   }
 }
+/*
+class sobreAplicativo extends StatelessWidget {
+  const sobreAplicativo({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: const Text('AlertDialog description'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text('Sobre'),
+    );
+  }
+}
+*/
+
+
+
+/*
 sobreAplicativo(context) 
 { 
     // configura o button
@@ -130,5 +233,5 @@ sobreAplicativo(context)
     },
   );
 }
-
+*/
 
