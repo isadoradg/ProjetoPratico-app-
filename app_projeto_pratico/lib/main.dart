2e19d94b9763e241d0096353d7cb9fd5bcf9e259
isadoradg <isahdomingos011@gmail.com>
2023-04-19 19:57:57 -0300

    Insersão dos caminhos para as telas de perfil, receitas e receitas-detalhes

--- app_projeto_pratico/lib/main.dart
+++ app_projeto_pratico/lib/main.dart
@@ -3,6 +3,9 @@
 import 'package:app_projeto_pratico/view/tela_cadastro.dart';
 import 'package:app_projeto_pratico/view/tela_esqueci_senha.dart';
 import 'package:app_projeto_pratico/view/tela_login.dart';
+import 'package:app_projeto_pratico/view/tela_perfil.dart';
+import 'package:app_projeto_pratico/view/tela_receitas.dart';
+import 'package:app_projeto_pratico/view/tela_receitas_detalhes.dart';
 import 'package:app_projeto_pratico/view/tela_sobre.dart';
 import 'view/tela_principal.dart';
 import 'package:flutter/material.dart';
@@ -21,6 +24,9 @@ void main() {
         '/trocar-senha':(context) => TelaEsqueciSenha(),
         '/inicial':(context) => TelaLogin(),
         '/sobre':(context) => TelaSobreApp(),
+        '/perfil':(context) => TelaPerfil(),
+        '/receitas':(context) => TelaReceitas(),
+        '/receitas-detalhes':(context) => TelaReceitasDetalhes(),
       },
       
     )
