78d153df00bbadf991dc53710cfadbeaa216c34a
isadoradg <isahdomingos011@gmail.com>
2023-04-19 20:05:17 -0300

    Alteração do que aparece na tela (pesquisa, receita e favoritos) e menu (pesquisa, receitas, favoritos, perfil e sobre)

--- app_projeto_pratico/lib/view/tela_login.dart
+++ app_projeto_pratico/lib/view/tela_login.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: prefer_const_constructors, camel_case_types
 
-import 'package:app_projeto_pratico/view/tela_principal.dart';
 import 'package:flutter/material.dart';
 
-//
-// Tela inicial após o login
-//
+// tela inicial após o login
 class TelaLogin extends StatefulWidget {
   const TelaLogin({super.key});
 
@@ -72,6 +69,7 @@ class _TelaLoginState extends State<TelaLogin> {
               ),
               onPressed: () {
                 //Navigator.pop(context);
+                Navigator.pushNamed(context, '/receitas');
               }, 
               child: const Text("Receitas"),
             ),
@@ -95,7 +93,7 @@ class _TelaLoginState extends State<TelaLogin> {
               height: 10,
             ),
 
-            ElevatedButton(
+            /*ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.white,
                 foregroundColor: Colors.black,
@@ -121,7 +119,7 @@ class _TelaLoginState extends State<TelaLogin> {
                 Navigator.pushNamed(context, '/sobre');
               },
               child: const Text("Sobre"), 
-            ),
+            ),*/
 
             
       
@@ -132,45 +130,38 @@ class _TelaLoginState extends State<TelaLogin> {
       drawer: Drawer(
           child: ListView(
             children: <Widget>[
-              UserAccountsDrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255,235,196,127),
-                ),
-                accountName: Text("Taylor Scott"),
-                accountEmail: Text("tscott@gmail.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Colors.white,//Color.fromARGB(255,235,196,127),
-                    /*Theme.of(context).platform == TargetPlatform.iOS
-                    ? Color.fromARGB(255,235,196,127): Colors.black,*/
-                    child: Text("T", style: 
-                      TextStyle(fontSize: 40.0, color: Colors.black),
-                    ),
-                ),
+              ListTile(
+                //leading: Icon(Icons.search)),
+                title: Text("Pesquisar Receita"),
+                trailing: Icon(Icons.search),
+                onTap: () {}
               ),
               ListTile(
-                  //leading: Icon(Icons.search)),
-                  title: Text("Pesquisar Receita"),
-                  //subtitle: Text("Perfil do usuário..."),
-                  trailing: Icon(Icons.search),
-                  onTap: () {
-                    //Navigator.pop(context);
-                  }),
+                leading: Icon(Icons.cookie_outlined),
+                title: Text("Receitas"),
+                onTap: () {
+                  Navigator.pushNamed(context, '/receitas');
+                }
+              ),
               ListTile(
-                  leading: Icon(Icons.star),
-                  title: Text("Favoritos"),
-                  //subtitle: Text("meus favoritos..."),
-                  //trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    //debugPrint('toquei no drawer');
-                  }),
+                leading: Icon(Icons.star),
+                title: Text("Favoritos"),
+                onTap: () {}
+              ),
               ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Text("Perfil"),
-                  //subtitle: Text("Perfil do usuário..."),
-                  //trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    //Navigator.pop(context);
-                  }),
+                leading: Icon(Icons.account_circle),
+                title: Text("Perfil"),
+                onTap: () {
+                  Navigator.pushNamed(context, '/perfil');
+                }
+              ),
+              ListTile(
+                leading: Icon(Icons.book_outlined),
+                title: Text("Sobre"),
+                onTap: () {
+                  Navigator.pushNamed(context, '/sobre');
+                }
+              ),
               
             ],
           ),
@@ -179,59 +170,4 @@ class _TelaLoginState extends State<TelaLogin> {
     );
   }
 }
-/*
-class sobreAplicativo extends StatelessWidget {
-  const sobreAplicativo({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('AlertDialog Title'),
-          content: const Text('AlertDialog description'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      ),
-      child: const Text('Sobre'),
-    );
-  }
-}
-*/
-
-
-
-/*
-sobreAplicativo(context) 
-{ 
-    // configura o button
-  Widget okButton = ElevatedButton(
-    child: Text("OK"),
-    onPressed: () {
-      Navigator.of(context).pop();
-    },
-  );
-  // configura o  AlertDialog
-  AlertDialog alerta = AlertDialog(
-    title: Text("Sobre o App"),
-    content: Text("~ explicações gerais sobre o trabalho ~"),
-    actions: [
-      okButton,
-    ],
-  );
-  // exibe o dialog
-  showDialog(
-    context: context,
-    builder: (context) {
-      return alerta;
-    },
-  );
-}
-*/
 
